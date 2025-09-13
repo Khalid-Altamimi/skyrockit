afed6a3c8992bc956f5ca146f1dbb5fa190a0733
Khalid Altamimi <khalida.altamimi71@gmail.com>
2025-09-13 14:16:14 +0300

    only edit and update left

--- controllers/applications.js
+++ controllers/applications.js
@@ -37,4 +37,30 @@ router.post('/', async (req, res) => {
   }
   })
 
+  router.get('/:applicationId', async (req, res) => {
+    try{
+      const currentUser = await User.findById(req.session.user._id);
+      const application = currentUser.applications.id(req.params.applicationId); 
+      res.render('applications/show.ejs', {
+        application: application,
+      });
+  } catch (error) {
+    console.log(error)
+    res.redirect('/');
+  }
+});
+
+router.delete('/:applicationId', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    currentUser.applications.id(req.params.applicationId).deleteOne();
+     await currentUser.save();
+     res.redirect(`/users/${currentUser._id}/applications`);
+     } catch (error) {
+      console.log(error);
+      res.redirect('/');
+  }
+  });
+
+
 module.exports = router;
